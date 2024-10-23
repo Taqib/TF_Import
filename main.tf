4214627a02f88a8b479e33f71a59c35aed110c1c
root <shamsunnabitaqib@gmail.com>
2024-10-23 06:23:54 +0000

    assd

--- main.tf
+++ main.tf
@@ -4,4 +4,10 @@ provider "aws" {
 
 resource "aws_vpc" "imported_vpc" {
   # Arguments will be added after import
+     cidr_block                           = "10.0.0.0/16"
+     enable_dns_support                   = true
+     tags                                 = {
+         Name = "my-vpc"
+     }
+
 }
